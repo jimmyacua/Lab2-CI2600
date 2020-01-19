8a8ac1a97511d86811f2fe4d25adefade07348ef
jimmyacua <jimmyacua@hotmail.com>
2020-01-19 17:14:08 -0600

    todo listo

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import csv
 import torch
 import pandas as pd
+from matplotlib import pyplot as plt
+import torch.optim as optim
+import math
 
 
 def read_csv(filename, col1, col2):
@@ -12,7 +15,7 @@ def read_csv(filename, col1, col2):
     return tensor1, tensor2
 
 
-def model(w, x, b):
+def model(x, w, b):
     y = w * x + b
     return y
 
@@ -41,18 +44,54 @@ def grad_fn(t_u, t_c, t_p, w, b):
     return torch.stack([dloss_dw.mean(), dloss_db.mean()])
 
 
-def training(n, w, b, alpha, x, y):
+def training(n, w, b,  alpha, x, y):
     for i in range(0, n):
-        t_p = model(w, x, b)
+        '''if w.grad is not None:
+            w.grad.zero_()
+        if b.grad is not None:
+            b.grad.zero_()'''
+
+        t_p = model(x, w, b)
         loss = loss_fn(t_p, y)
-        grad = grad_fn(x, y, t_p, w, b)
+        optimizer = optim.SGD([w, b], lr=alpha)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
+        # ya no se usa por el auto grad
+        '''grad = grad_fn(x, y, t_p, w, b)
         w = w - alpha*grad[0]
-        b = b - alpha * grad[1]
+        b = b - alpha * grad[1]'''
 
-        print('Epoca %d, Loss %f' % (i, float(loss)))
+        #w = (w - learning_rate * w.grad).detach().requires_grad_()
+        #b = (b - learning_rate * b.grad).detach().requires_grad_()
+
+        if n % 500 == 0:
+            print('Epoch %d, Loss %f' % (i, float(loss)))
+        '''print('Epoch %d, Loss %f' % (i, float(loss)))
+        print('w: ', w, ', b:', b)
+        print('Grad: ', grad)'''
 
     return w, b
+
+
+def model_nonlin(a, x, b, c):
+    y = a*x**b+c  # para usar esta formula usar le = 1e-6
+    # y = a*(math.e**(b*x))+c  # es mejor la primera
+    return y
+
+
+def training_nonlin(n, w, b, c, alpha, x, y):
+    for i in range(0, n):
+        t_p = model_nonlin(w, x, b, c)
+        loss = loss_fn(t_p, y)
+        optimizer = optim.SGD([w, b], lr=alpha)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if n % 500 == 0:
+            print('Epoch %d, Loss %f' % (i, float(loss)))
 
 
 if __name__ == "__main__":
@@ -81,13 +120,16 @@ if __name__ == "__main__":
     aux = indixes_tensor2[twentyPercent2:]
     trainingSet_tensor2 = rest2[aux]
 
-    w = torch.ones(1)
-    b = torch.zeros(1)
-    learning_rate = 1e-4
+    w = torch.ones(1, requires_grad=True)
+    b = torch.zeros(1, requires_grad=True)
+    c = torch.zeros(1, requires_grad=True)
+    learning_rate = 1e-9
 
-    #print(trainingSet_tensor1.size())
-    #print(w.size())
-
-    #print(model(w, trainingSet_tensor1, b))
-    training(1000, w, b, learning_rate, trainingSet_tensor1, trainingSet_tensor2)
-
+    xn = (trainingSet_tensor1 - trainingSet_tensor1.mean())/trainingSet_tensor1.size()[0]# para normalizar la entrada.
+    #xn = 0.1 * trainingSet_tensor1
+    t_p = model(xn, w, b)
+            #t_u = trainingSet_tensor1, tc = trainingSet_tensor2
+    plt.plot(trainingSet_tensor1.numpy(), t_p.detach().numpy())
+    plt.plot(trainingSet_tensor1.numpy(), trainingSet_tensor2.numpy(), 'o')
+    #training(5000, w, b, learning_rate, xn, trainingSet_tensor2)
+    training_nonlin(2000, w, b, c, learning_rate, trainingSet_tensor1, trainingSet_tensor2)
